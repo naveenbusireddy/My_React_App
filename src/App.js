8be20ad28a6d94dec910de66d2fd8f78bb01564b
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2022-03-12 17:30:17 +0530

    upto components & props final

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 // import logo from './logo.svg';
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
-  const expense = [
-    { title : "Car Insurance",
-      amount : 294.63,
-      date : new Date(2022, 2, 10)  
+  const expensesList = [
+    { 
+      title: "Car Insurance", 
+      amount: 294.63, 
+      date: new Date(2022, 2, 10) 
     },
-    { title : "Bike Insurance",
-      amount : 105.63,
-      date : new Date(2021, 11, 30),  
+    { 
+      title: "Bike Insurance", 
+      amount: 105.63, 
+      date: new Date(2021, 11, 30) 
     },
-    { title : "Accidental Insurance",
-      amount : 94.63,
-      date : new Date(2020, 7, 30),  
+    {
+      title: "Accidental Insurance",
+      amount: 94.63,
+      date: new Date(2020, 7, 30),
     },
-
-  ]
+  ];
   return (
     <div>
       <h2>Welcome to React App.</h2>
-      <ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date}></ExpenseItem>
-      <ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date}></ExpenseItem>
-      <ExpenseItem title={expense[2].title} amount={expense[2].amount} date={expense[2].date}></ExpenseItem>
+      <Expenses items={expensesList}></Expenses>
     </div>
   );
 }
--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,13 +1,15 @@
+import './ExpenseDate.css';
+
 function ExpenseDate(props) {
   const month = props.expenseDate.toLocaleDateString("en-US", { month: "long" }); //date.toLocaleDateString('en-US', options)
   const day = props.expenseDate.toLocaleDateString("en-US", { day: "2-digit" });
   const year = props.expenseDate.toLocaleDateString("en-US", { year: "numeric" });
 
   return (
-    <div>
-      <div>{day}</div>
-      <div>{month}</div>
-      <div>{year}</div>
+    <div className='expense-date'>
+      <div className='expense-date__day'>{day}</div>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__year'>{year}</div>
     </div>
   );
 }
