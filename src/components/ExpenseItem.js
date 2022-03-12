e60951f8257658832266339a033c7c649dc36207
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2022-03-12 23:00:31 +0530

    up to useState and Click EventListener.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import React, { useState } from "react";
 
 function ExpenseItem(props) {
   // const expenseTitle = "Car Insurance";
@@ -10,18 +11,20 @@ function ExpenseItem(props) {
   // const day = props.date.toLocaleDateString('en-US', {day: "2-digit"});
   // const year = props.date.toLocaleDateString('en-US', {year: "numeric"});
 
-  let title = props.title;
+  useState();
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    title = "update";
-    console.log(title); 
-  }
+    setTitle("Updated");
+    console.log(title);
+  };
   return (
     <div className="expense-item">
       <ExpenseDate expenseDate={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickHandler} >Change Title</button>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </div>
   );
