0e37493f8480050d39d7846c14ef589a674e9db6
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2022-03-12 19:09:15 +0530

    Using Events & useState in React App.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -9,12 +9,19 @@ function ExpenseItem(props) {
   // const month = props.date.toLocaleDateString('en-US', {month : "long"});  //date.toLocaleDateString('en-US', options)
   // const day = props.date.toLocaleDateString('en-US', {day: "2-digit"});
   // const year = props.date.toLocaleDateString('en-US', {year: "numeric"});
+
+  let title = props.title;
+  const clickHandler = () => {
+    title = "update";
+    console.log(title); 
+  }
   return (
     <div className="expense-item">
       <ExpenseDate expenseDate={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler} >Change Title</button>
       </div>
     </div>
   );
