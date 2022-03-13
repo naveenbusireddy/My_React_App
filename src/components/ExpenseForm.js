fa20d77e4e4825a1bda051e14c29900670d8871a
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2022-03-13 09:58:18 +0530

    Form update

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,25 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('')
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-    const titleChangeHandler = (event) => 
-    {
-        setEnteredTitle(event.target.value);
-        // console.log(event.target.value);
-    };    
-    const amountChangeHandler = (event) => 
-    {
-        setEnteredAmount(event.target.value);
-    };
-    const dateChangeHandler = (event) => 
-    {
-        setEnteredDate(event.target.value);
-    };
-
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    // console.log(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
 
   return (
     <form>
@@ -27,18 +23,23 @@ const ExpenseForm = () => {
         <div className="new-expense__controls"></div>
         <div className="new-expense__control label">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}/>
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control label">
           <label>Amount</label>
-          <input type="number" onChange={amountChangeHandler}/>
+          <input type="number" onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control label">
           <label>Date</label>
-          <input type="date" min='2019-01-01' max='2022-03-11' onChange={dateChangeHandler}/>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-03-11"
+            onChange={dateChangeHandler}
+          />
         </div>
         <div>
-            <button type="submit">Add Expenses</button>
+          <button type="submit">Add Expenses</button>
         </div>
       </div>
     </form>
