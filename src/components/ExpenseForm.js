a318492e912598ae66f39e5cbccf1ffed818d14f
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2022-03-14 10:24:15 +0530

    Entering the data in form fields.

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -6,19 +6,49 @@ const ExpenseForm = () => {
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
+
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
     // console.log(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
   };
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   };
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    
+    console.log(expenseData);
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div>
         <div className="new-expense__controls"></div>
         <div className="new-expense__control label">
@@ -27,7 +57,7 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control label">
           <label>Amount</label>
-          <input type="number" onChange={amountChangeHandler} />
+          <input type="number" min='0.01' step='0.01' onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control label">
           <label>Date</label>
