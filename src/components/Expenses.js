599f27cbb5fd891102e533258ef85d4a3e9c3aa5
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2022-03-15 20:40:42 +0530

    Feature List fall back option.

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+// import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -14,18 +15,6 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No Expenses Found.</p>;
-  if(ExpensesFilteredYear.length > 0) {
-    expensesContent = ExpensesFilteredYear.map((expense) => (
-      <ExpenseItem
-        key = {expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
-
   return (
     <div>
       <div className="expenses">
@@ -33,7 +22,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {expensesContent}
+      <ExpensesList items={ExpensesFilteredYear} />
 
       {/* <ExpenseItem
         title={props.items[0].title}
